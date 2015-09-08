c3e2b97990c8d1208ebe9451bf6dc6b085a29373
Alex Gyoshev <alex@gyoshev.net>
2015-09-08 19:06:01 +0800

    Suggest blending functions, remove HSL

--- test/main.js
+++ test/main.js
@@ -4,18 +4,37 @@ var suggest = require("../lib/main").suggest;
 describe("Cuttle", function(){
   describe("#suggest()", function(){
     var suggestion;
-    function firstFormat(from, to) {
-        var suggestions = suggest(from, to);
+    function suggestions(from, to) {
+        var result = suggest(from, to);
 
-        if (!suggestions.length) {
+        if (!result.length) {
             throw new Error("Nothing suggested!");
         }
 
-        return suggestions[0].format;
+        return result;
+    }
+    function firstFormat(from, to) {
+        return suggestions(from, to)[0].format;
+    }
+    function allFormats(from, to) {
+        return suggestions(from, to).map(function(x) {
+            return x.format;
+        });
+    }
+    function allComplexities(from, to) {
+        var functionName = /^(.*)\(/i;
+        return suggestions(from, to).reduce(function(result, x) {
+            var type = functionName.exec(x.format)[1];
+            result[type] = x.complexity;
+            return result;
+        }, {});
     }
     assert.contains = function(haystack, needle, message) {
         assert(haystack.indexOf(needle) > -1, message ||
                (haystack + " does not contain " + needle));
+    };
+    assert.lt = function(a, b, message) {
+        assert(a < b, message || (a + " is not less than " + b));
     };
     it("throws when called without input", function(){
       assert.throws(suggest);
@@ -29,21 +48,25 @@ describe("Cuttle", function(){
       assert.contains(suggestion, "@input");
     });
     it("should suggest lighten", function() {
-      suggestion = firstFormat("000000", "030303");
+      suggestion = allFormats("000000", "030303");
       assert.contains(suggestion, "lighten(@input, 1%)");
 
-      suggestion = firstFormat("000000", "050505");
+      suggestion = allFormats("000000", "050505");
       assert.contains(suggestion, "lighten(@input, 2%)");
     });
     it("should suggest darken", function() {
-      suggestion = firstFormat("cccccc", "c9c9c9");
+      suggestion = allFormats("cccccc", "c9c9c9");
       assert.contains(suggestion, "darken(@input, 1%)");
 
-      suggestion = firstFormat("cccccc", "c4c4c4");
+      suggestion = allFormats("cccccc", "c4c4c4");
       assert.contains(suggestion, "darken(@input, 3%)");
     });
+    it("should suggest darken as a simpler format", function() {
+      var complexities = allComplexities("cccccc", "c4c4c4");
+      assert.lt(complexities.darken, complexities.multiply);
+    });
     it("should suggest desaturate", function() {
-      suggestion = firstFormat("80e619", "80cc33");
+      suggestion = allFormats("80e619", "80cc33");
       assert.contains(suggestion, "desaturate(@input, 20%)");
     });
     it("should suggest saturate", function() {
@@ -70,9 +93,47 @@ describe("Cuttle", function(){
       suggestion = firstFormat("80f20d", "808080");
       assert.contains(suggestion, "greyscale(@input)");
     });
-    it("should suggest hsl transform", function() {
-      suggestion = firstFormat("80f20d", "f820d0");
-      assert.contains(suggestion, "hsl(hue(@input * ), saturation(@input * ), lightness(@input) * )");
+    it("should suggest multiply", function() {
+      suggestion = allFormats("ff6600", "331400");
+      assert.contains(suggestion, "multiply(@input, #333333)");
+
+      suggestion = allFormats("ff6600", "cc5200");
+      assert.contains(suggestion, "multiply(@input, #cccccc)");
+    });
+    it("should suggest screen", function() {
+      suggestion = allFormats("ff6600", "ffa366");
+      assert.contains(suggestion, "screen(@input, #666666)");
+
+      suggestion = allFormats("ff6600", "ffc299");
+      assert.contains(suggestion, "screen(@input, #999999)");
+    });
+    it("should suggest overlay", function() {
+      suggestion = allFormats("ff6600", "ff2900");
+      assert.contains(suggestion, "overlay(@input, #333333)");
+
+      suggestion = allFormats("ff6600", "ffa300");
+      assert.contains(suggestion, "overlay(@input, #cccccc)");
+    });
+    it("should suggest softlight", function() {
+      suggestion = allFormats("ff6600", "ff5a00");
+      assert.contains(suggestion, "softlight(@input, #666666)");
+
+      suggestion = allFormats("ff6600", "ff8a00");
+      assert.contains(suggestion, "softlight(@input, #cdcdcd)");
+    });
+    it("should suggest difference", function() {
+      suggestion = allFormats("ff6600", "cc3333");
+      assert.contains(suggestion, "difference(@input, #333333)");
+
+      suggestion = allFormats("ff6600", "663399");
+      assert.contains(suggestion, "difference(@input, #999999)");
+    });
+    it("should suggest exclusion", function() {
+      suggestion = allFormats("ff6600", "cc7033");
+      assert.contains(suggestion, "exclusion(@input, #333333)");
+
+      suggestion = allFormats("ff6600", "668599");
+      assert.contains(suggestion, "exclusion(@input, #999999)");
     });
   });
 });
